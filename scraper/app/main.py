53730d3db2dbae3d22798555fdea80268741bb08
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2021-06-06 01:26:39 +0000

    fix response model

--- scraper/app/main.py
+++ scraper/app/main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    """Basic test for the webapp.
+    """Show hellow world page.
 
     Returns
     -------
@@ -23,14 +23,20 @@ def read_root():
     return {"Hello": "scraper test"}
 
 
-@app.get("/rentfaster/listings/{city_id}/page/{page}")
+@app.get(
+    "/rentfaster/listings/{city_id}/page/{page}",
+    response_model=List[rfaster.RFasterListingSummary],
+)
 def get_rentfaster_listings_page(
     city_id: int = 1, page: int = 0
 ) -> List[rfaster.RFasterListingSummary]:
     return rfaster.get_listings_page(city_id, page)
 
 
-@app.get("/rentfaster/listings/{city_id}/all")
+@app.get(
+    "/rentfaster/listings/{city_id}/all",
+    response_model=List[rfaster.RFasterListingSummary],
+)
 def get_all_rentfaster_listings(
     city_id: int = 1,
 ) -> List[rfaster.RFasterListingSummary]:
