f376796512d8898f4170edd17203063f84b0a3d8
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2021-11-01 19:23:31 +0000

    run in analyst mode for isochrones

--- scripts/tripplanner.sh
+++ scripts/tripplanner.sh
@@ -3,15 +3,17 @@
 echo "Be sure to run download_osm_data.py and download_transit_data.py first"
 
 # If we don't have the graph generated create it
-if [[ ! -f $PWD/../data/isochrone/commute/Graph.obj ]]; then
+if [[ ! -f $PWD/../data/isochrone/commute/calgary/Graph.obj ]]; then
+  echo "No graph built, building that first"
   docker run \
     -v $PWD/../data/isochrone/commute:/var/otp/graphs \
     -e JAVA_OPTIONS=-Xmx4G \
     urbica/otp --build /var/otp/graphs/calgary
 fi
 
+echo "running the actual planner"
 docker run \
   -p 8080:8080 \
   -v $PWD/../data/isochrone/commute:/var/otp/graphs \
   -e JAVA_OPTIONS=-Xmx4G \
-  urbica/otp --server --autoScan --verbose
+  urbica/otp --server  --analyst --autoScan --verbose
