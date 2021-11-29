f401fd3041a0c0d059ed8a1447c26300907cd1c1
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2021-11-29 21:02:20 +0000

    add dropbox upload functionality

--- scripts/daily_mls.py
+++ scripts/daily_mls.py
@@ -2,7 +2,9 @@
 from wheretolive.mls.ingest import update_mls_postgis
 from wheretolive.mls.parse import parse_scrapes
 from wheretolive.mls.scrape import scrape_all
+from wheretolive.mls.upload_candidates import upload_candidate
 
 scrape_all()
 parse_scrapes()
 update_mls_postgis()
+upload_candidate("ian")
