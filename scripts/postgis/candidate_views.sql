cbc77b497272e2da219a7a76c8c4b285609a7991
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2021-11-30 00:22:53 +0000

    add parents, clean up mine

--- scripts/postgis/candidate_views.sql
+++ scripts/postgis/candidate_views.sql
@@ -9,6 +9,7 @@ SELECT
   scrape_dt,
   listing_description,
   downtown_walk_time,
+  downtown_walk_transit_time,
   gf_work_car_time,
   brother_car_time
 FROM mls_wide
@@ -17,5 +18,34 @@ WHERE
   AND bedrooms >= 2
   AND gf_work_car_30 = TRUE
   AND brother_car_40 = TRUE
+  AND bathrooms >= 2
+  AND (
+      parking LIKE '%Garage%'
+      OR parking LIKE '%Underground%'
+  )
+  AND listing_type NOT IN ('Duplex', 'Mobile Home')
+  AND downtown_walk_transit_40
 ORDER BY mls_insert_dt DESC
+;
+DROP VIEW IF EXISTS parents_candidates;
+CREATE OR REPLACE VIEW parents_candidates AS
+SELECT
+  mls_number,
+  link,
+  price,
+  mls_insert_dt,
+  scrape_dt,
+  listing_description,
+  brother_car_time
+  FROM mls_wide
+  WHERE
+    price BETWEEN 200000 AND 500000
+    AND bathrooms >= 2
+    AND bedrooms >= 2
+    AND (
+        parking LIKE '%Garage%'
+        OR parking LIKE '%Underground%'
+    )
+    AND brother_car_20 = TRUE
+  ORDER BY mls_insert_dt DESC
 ;
