d31cc0453904fc0855bddb56bc58f22e7133b45a
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2021-12-01 01:24:58 +0000

    add stuff for Jill

--- scripts/postgis/candidate_views.sql
+++ scripts/postgis/candidate_views.sql
@@ -11,7 +11,8 @@ SELECT
   downtown_walk_time,
   downtown_walk_transit_time,
   gf_work_car_time,
-  brother_car_time
+  brother_car_time,
+  to_bc_car_time
 FROM mls_wide
 WHERE
   price BETWEEN 300000 AND 650000
@@ -48,4 +49,36 @@ SELECT
     )
     AND brother_car_20 = TRUE
   ORDER BY mls_insert_dt DESC
+;
+DROP VIEW IF EXISTS jill_candidates;
+CREATE OR REPLACE VIEW jill_candidates AS
+SELECT
+  mls_number,
+  link,
+  price,
+  mls_insert_dt,
+  scrape_dt,
+  listing_description,
+  downtown_walk_time,
+  downtown_walk_transit_time,
+  downtown_car_time,
+  to_bc_car_time,
+  momma_jill_car_time
+FROM mls_wide
+WHERE
+  price BETWEEN 900000 AND 1500000
+  AND bedrooms_above >= 3
+  AND bathrooms > 3
+  AND parking_spaces >= 2
+  AND (
+    (downtown_car_25 = TRUE)
+    OR (downtown_walk_30 = TRUE)
+  )
+  AND (
+    (listing_type = 'House')
+    OR (listing_type is NULL)
+  )
+  AND momma_jill_car_10 = False
+  AND sq_feet_in >= 2000
+ORDER BY mls_insert_dt DESC
 ;
