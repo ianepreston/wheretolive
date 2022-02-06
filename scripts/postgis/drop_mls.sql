cf590c9bd26386ca08e63ecbc6b762ff12c4aebc
Ian Preston <17241371+ianepreston@users.noreply.github.com>
2022-02-06 21:47:22 +0000

    fix up drop script to reflect actual tables and views

--- scripts/postgis/drop_mls.sql
+++ scripts/postgis/drop_mls.sql
@@ -1,6 +1,6 @@
 -- Drop the mls record and staging tables. Useful for a hard reset while developing
-DROP VIEW IF EXISTS mls_update_stage_view;
-DROP VIEW IF EXISTS mls_insert_stage_view;
-DROP VIEW IF EXISTS mls_update_backfill_stage_view;
-DROP TABLE IF EXISTS mls_staging;
+DROP MATERIALIZED VIEW IF EXISTS mls_wide CASCADE;
+DROP VIEW IF EXISTS mls_flood;
+DROP VIEW IF EXISTS mls_commutes;
+DROP VIEW IF EXISTS mls_grocery;
 DROP TABLE IF EXISTS mls;
